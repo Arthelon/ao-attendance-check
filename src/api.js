05716ed6b83c9d97e0e7e39d6178f2b49bcabef0
Arthelon <hsing.daniel@gmail.com>
2024-05-16 20:24:14 -0400

    change api urls for europe server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const undici = require("undici");
-const BASE_API_URL = "https://gameinfo.albiononline.com/api/gameinfo";
-const ALBION_BB_URL = "https://api.albionbb.com";
+const BASE_API_URL = "https://gameinfo-ams.albiononline.com/api/gameinfo"; //  https://gameinfo.albiononline.com/api/gameinfo for West
+const ALBION_BB_URL = "https://api-europe.albionbb.com"; // https://api.albionbb.com for West
 
 const getPlayerActivityStats = async (guildId, startDate, minPlayers) => {
   const url = new URL(`${ALBION_BB_URL}/stats/guilds/${guildId}`);
